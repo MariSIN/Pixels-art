2bdf3be7fecbd5475ea8f5090145068d2fceb554
Mariana Nascimento <mariana.sn.silva@gmail.com>
2022-08-02 16:34:49 -0300

    quarto requisito feito

---
+++ script.js
@@ -0,0 +1,9 @@
+const pixels = document.querySelector('#pixel-board');
+function addPixels(){
+    for(let i = 0; i < 25; i+=1){
+        const pixel = document.createElement('div');
+        pixel.className = 'pixel';
+        pixels.appendChild(pixel)
+    }
+}
+addPixels()
